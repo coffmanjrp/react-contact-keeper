db1f86a8e1ebbbc70a2240859878f3459def3de9
coffmanjrp <coffmanjrp@gmail.com>
2021-02-11 22:50:55 -0800

    create contact model and get contacts route

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+const auth = require('../middleware/auth');
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
 // @route   GET api/contacts
 // @desc    Get all users contacts
 // @access  Private
-router.get('/', (req, res) => {
-  res.send('Get all contacts');
+router.get('/', auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      data: -1,
+    });
+    res.json(contacts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('server error');
+  }
 });
 
 // @route   POST api/contacts
