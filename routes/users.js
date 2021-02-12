4a21781e229fcf89f95c7878ddd228068db0fbe2
coffmanjrp <coffmanjrp@gmail.com>
2021-02-11 20:45:23 -0800

    authenicate route

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,6 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator/check');
 const config = require('config');
-
 const User = require('../models/User');
 
 // @route   POST api/users
