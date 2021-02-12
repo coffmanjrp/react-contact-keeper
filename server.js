0de95b9e0f903e045d4cdf7129999964d52a8d8e
coffmanjrp <coffmanjrp@gmail.com>
2021-02-11 18:33:36 -0800

    create user model and validation

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Init middleware
+app.use(express.json({ extended: false }));
+
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
