77896b0912bb89de8683cf6f90820ac88299af16
coffmanjrp <coffmanjrp@gmail.com>
2021-02-14 15:03:40 -0800

    create auth context and initial states

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,23 +2,26 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Navbar } from './components/layout';
 import { Home, About } from './components/pages';
 import { ContactState } from './context/contact';
+import { AuthState } from './context/auth';
 import './assets/css/App.css';
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-            </Switch>
-          </div>
-        </>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </div>
+          </>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 };
 
---
+++ client/src/context/auth/index.js
@@ -0,0 +1,3 @@
+export { default as AuthContext } from './authContext';
+export { default as authReducer } from './authReducer';
+export { default as AuthState } from './AuthState';
