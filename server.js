358e835695c758faae936cc54d4b1fab25b0459c
coffmanjrp <coffmanjrp@gmail.com>
2021-02-15 10:20:00 -0800

    set settings for deploying

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const colors = require('colors');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -15,9 +16,15 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
-app.get('/', (req, res) => {
-  res.json({ msg: 'Welcome to the Contact Keeper Api' });
-});
+// Serve static assets if production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
