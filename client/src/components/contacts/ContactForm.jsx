cd777851c54caa665a100b1002bd23b4d66a5be5
coffmanjrp <coffmanjrp@gmail.com>
2021-02-15 10:03:21 -0800

    implement update contacts from DB

--- client/src/components/contacts/ContactForm.jsx
+++ client/src/components/contacts/ContactForm.jsx
@@ -43,12 +43,7 @@ const ContactForm = () => {
       updateContact(contact);
     }
 
-    setContact({
-      name: '',
-      email: '',
-      phone: '',
-      type: 'personal',
-    });
+    clearAll();
   };
 
   const clearAll = () => {
